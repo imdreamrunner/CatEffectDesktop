2b4caf5fd35f954fb072a8c8c721e99009026c5f
Zhou Xinzi <imdreamrunner@gmail.com>
2013-10-08 17:30:24 +0800

    Add Auth Login

--- src/cams/LoginFormController.java
+++ src/cams/LoginFormController.java
@@ -4,6 +4,7 @@
  */
 package cams;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -12,9 +13,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import jfx.messagebox.MessageBox;
 
 /**
  *
@@ -25,7 +29,32 @@ public class LoginFormController implements Initializable {
     private Stage selfStage;
     
     @FXML
+    TextField username;
+    
+    @FXML
+    TextField password;
+    
+    @FXML
+    ChoiceBox choiceType;
+    
+    @FXML
     private void login(ActionEvent event) throws Exception {
+        int type = 0;
+        if (choiceType.getValue().toString().equals("OFS Staff")) {
+            type = 1;
+        }
+        boolean success = ServerInterface.login(type, username.getText(), password.getText());
+        if (success) {
+            openMainForm();
+        } else {
+            MessageBox.show(selfStage,
+                ServerInterface.getErrorMessage(),
+                "Login Fails",
+                MessageBox.ICON_INFORMATION);
+        }
+    }
+    
+    private void openMainForm() throws IOException {
         selfStage.close();
         
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainForm.fxml"));
--- src/cams/ServerInterface.java
+++ src/cams/ServerInterface.java
@@ -4,6 +4,26 @@
  */
 package cams;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  *
  * @author dreamrunner
@@ -11,11 +31,91 @@ package cams;
 public class ServerInterface {
     private static String serverAddress = "http://localhost:9000";
     
-    private static String username, password;
-    public static boolean login(String newUsername, String newPassword) {
+    
+    private static String username, password, errorMessage;
+    private static int managerType;
+    
+    public static boolean login(int type, String newUsername, String newPassword) {
+        managerType = type;
         username = newUsername;
         password = newPassword;
-        return true;
+        try {
+            boolean success = postRequest();
+            if (success) {
+                return true;
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ServerInterface.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+    private static boolean postRequest() throws UnsupportedEncodingException, IOException, JSONException {
+        HttpClient httpclient = new DefaultHttpClient();
+        
+        String url;
+        if (managerType == 0) {
+            url = getUrl("/stall/auth");
+        } else {
+            url = getUrl("/system/auth");
+        }
+        
+        HttpPost httppost = new HttpPost();
+
+        // Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("auth_username", username));
+        params.add(new BasicNameValuePair("auth_password", password));
+        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+        //Execute and get the response.
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+            try {
+                String result = getStringFromInputStream(instream);
+                JSONObject jsonObj = new JSONObject(result);
+                String errorCode = jsonObj.get("error").toString();
+                if (errorCode.equals("0")) {
+                    return true;
+                } else {
+                    errorMessage = jsonObj.get("message").toString();
+                }
+            } finally {
+                instream.close();
+            }
+        }
+        return false;
+    }
+    
+    private static String getStringFromInputStream(InputStream is) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+
     }
     
     public static void setServerAddress (String newServerAddress) {
@@ -27,6 +127,9 @@ public class ServerInterface {
     }
     public static String getPassword() {
         return "admin";
+    }
+    public static String getErrorMessage() {
+        return errorMessage;
     }
     public static String getUrl(String url) {
         String ret = serverAddress + url;
